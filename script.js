10ba1fa1abb8502202de317729bebaac37edbc80
HamzaArif538 <sardarhamza538@gmail.com>
2022-03-19 21:13:17 +0500

    completed

--- script.js
+++ script.js
@@ -3,4 +3,22 @@ const body = document.querySelector("body"),
     toggle = body.querySelector(".toggle"),
     searchBtn = body.querySelector(".search-box"),
     modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".toggle-switch")
+    modeText = body.querySelector(".mode-text")
+
+    
+    toggle.addEventListener("click" , () => {
+        sidebar.classList.toggle("close");
+    });
+    
+    modeSwitch.addEventListener("click" , () => {
+        body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerHTML = "Light Mode"
+        }
+        else {
+            modeText.innerHTML= "Dark Mode"
+        }
+    });
+
+
